32aa4f8bfe776ceae9b86f0349741ec5de88592e
Eomjihee <eomjihee.code@gmail.com>
2020-12-08 20:48:37 +0900

    기본 프로젝트 생성 / Spring Boot, Gradle, JPA

---
+++ Petaku/src/main/java/com/petaku/PetakuApplication.java
@@ -0,0 +1,13 @@
+package com.petaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetakuApplication.class, args);
+	}
+
+}
